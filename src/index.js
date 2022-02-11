d80b5fa8f25314a6527cc5f2db8a6944b5efe87d
Eric Crudup <briefcasemanx@gmail.com>
2022-02-11 12:28:46 -0800

    achieves api call functionality without error messaging

--- src/index.js
+++ src/index.js
@@ -6,18 +6,22 @@ import ExchangeService from './js/exchange-service.js';
 
 
 
-function conversion(usd, conversionRate){
+function convertFromUsd(usd, conversionRate)  {
+
+  return Math.round(((usd * conversionRate) + Number.EPSILON) * 100) / 100;
 }
+
 $(document).ready(function() {
-  
 
   $('#submit-usd').click(function() {
-    let usd= $('#usd-input').val();
-    let newCurrency = $('new-curr-input').val();
-    let promise = ExchangeService.convertUsdTo(usd, newCurrency);;
+    let usd= parseFloat( $('#usd-input').val() );
+    let newCurrency = $('#new-curr-input').val();
+    let promise = ExchangeService.getExchange(newCurrency);
     promise.then(function(response) {
       const body = JSON.parse(response);
-      $('.show-conversion').text(`with ${usd} you can get ${body}%`);
+      const convertedAmt = convertFromUsd( usd, parseFloat(body.conversion_rate) );
+      $('#show-conversion').text(`with $${usd}, you can get `+ convertedAmt);
+      $('#show-conversion').show();
     }, function(error) {
       $('.show-errors').text(`There was an error processing your request: ${error}`);
     });
--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,15 +1,15 @@
 export default class ExchangeService {  
-  static convertUsdTo(currency) {
+  static getExchange(newCurrency) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${newCurrency}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
